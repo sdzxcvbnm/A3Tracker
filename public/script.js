2e325df05ad8e8b16bed5d5bd1a45f723d879ee4
sarah <sarahdang0208@gmail.com>
2023-06-04 15:50:33 +1000

    updated site with css, and html

--- public/script.js
+++ public/script.js
@@ -1,28 +1,30 @@
 // Setting up variables for our HTML elements using DOM selection
-const form = document.getElementById("taskform");
-const tasklist = document.getElementById("tasklist");
+const form = document.getElementById("cakeform");
+const cakelist = document.getElementById("cakelist");
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    console.log(form.elements.taskType.value)
+    console.log(form.elements.cakeType.value)
 
-    addTask(
-        form.elements.taskName.value,
-        form.elements.taskType.value,
-        form.elements.taskRate.value,
-        form.elements.taskTime.value,
-        form.elements.taskClient.value,
+    addCake(
+        form.elements.cakeName.value,
+        form.elements.cakeType.value,
+        form.elements.cakeFlavour.value,  
+        form.elements.cakeRate.value,
+        form.elements.cakeBakery.value,
+        form.elements.cakeComment.value,
     )
-    console.log(taskList)
+    console.log(entryList)
 })
 
-function displayTask(task) {
-    let item = document.createElement("li");
-    item.setAttribute("data-id", task.id);
-    item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
 
-    tasklist.appendChild(item);
+function displayCake(cake) {
+    let item = document.createElement("li");
+    item.setAttribute("data-id", cake.id);
+    item.innerHTML = `<p><strong>${cake.name}</strong><br>${cake.type}<br>${cake.flavour}<br>${cake.rate}<br>${cake.date}</p>`;
+
+    entrylist.appendChild(item);
 
     // Clear the value of the input once the task has been added to the page
     form.reset();
@@ -36,85 +38,49 @@ function displayTask(task) {
     // Listen for when the delete button is clicked
     delButton.addEventListener("click", function (event) {
 
-        taskList.forEach(function (taskArrayElement, taskArrayIndex) {
-            if (taskArrayElement.id == item.getAttribute('data-id')) {
-                taskList.splice(taskArrayIndex, 1)
+        entryList.forEach(function (cakeArrayElement, cakeArrayIndex) {
+            if (cakeArrayElement.id == item.getAttribute('data-id')) {
+                entryList.splice(cakeArrayIndex, 1)
             }
         })
 
         // Make sure the deletion worked by logging out the whole array
-        console.log(taskList)
+        console.log(entryList)
 
         item.remove(); // Remove the task item from the page when button clicked
         // Because we used 'let' to define the item, this will always delete the right element
 
     })
-
-
 }
 
 
 
-
-// Create an object called 'task'
-// Populate the properties based on the provided data model
-
-// Commented out now the object creation is included in the function
-
-// var task = {
-//   name: "Initial Sketches",
-//   type: "Concept Ideation",
-//   id: Date.now(),
-//   date: new Date().toISOString(),
-//   rate: 50,
-//   time: 5,
-//   client: "Google"
-// }
-
-// console.log(task);
+var entryList = [];
 
 
-// Create an array called 'taskList'
-var taskList = [];
 
-// Create a function called 'addTask'
-// Give the function input parameters for: name, type, rate, time, client
-// Paste your object definition from above in the function
-// Replace the property values with the input paramaters
-// Add the object to the taskList array
-
-function addTask(name, type, rate, time, client) {
+function addCake(name, type, flavour, rate, bakery, comment) {
 
     // Creating the object with the usual property:value syntax
-    // Create task object 
-    // let task = {
-    //   name: name,
-    //   type: type,
-    //   id: Date.now(),
-    //   date: new Date().toISOString(),
-    //   rate: rate,
-    //   time: time,
-    //   client: client
-    // }
-
     // Creating the object, directly passing in the input parameters
-    let task = {
+    let cake = {
         name,
         type,
         id: Date.now(),
         date: new Date().toISOString(),
+        flavour,        
         rate,
-        time,
-        client
+        bakery,
+        comment
     }
 
-    taskList.push(task);
-    displayTask(task);
+    entryList.push(cake);
+    displayCake(cake);
 
 }
 
 // Call the function with test values for the input paramaters
-addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
+addCake("Cake", "Sponge Cake", "Red Velvet", 5, "thumbs up");
 
 // Log the array to the console.
-console.log(taskList);
+console.log(entryList);
